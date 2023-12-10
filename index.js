e435c58de7ab94403a296a6313bcedd06c137248
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-10 19:10:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you ${names[i]} for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return names;
+}
+
+writeCards (names);
